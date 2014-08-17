b2b57f48e4a171982746ad166e5a84d32c61952b
Roman <roman@bogdan.training.local>
2014-08-17 16:26:06 +0300

    Odnoklassniki

---
+++ Odnoklassniki.h
@@ -0,0 +1,13 @@
+//
+//  Odnoklassniki.h
+//  Social Networks
+//
+//  Created by Roman on 8/17/14.
+//  Copyright (c) 2014 SoftServe Lv-120. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Odnoklassniki : UIViewController
+
+@end
---
+++ Odnoklassniki.m
@@ -0,0 +1,38 @@
+//
+//  Odnoklassniki.m
+//  Social Networks
+//
+//  Created by Roman on 8/17/14.
+//  Copyright (c) 2014 SoftServe Lv-120. All rights reserved.
+//
+
+#import "Odnoklassniki.h"
+
+@interface Odnoklassniki ()
+
+@end
+
+@implementation Odnoklassniki
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
