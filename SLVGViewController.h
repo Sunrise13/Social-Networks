b636684baa9749c663f071b47b0845e59d642ed4
conqueror1 <oles-bilyk@yandex.ua>
2014-08-17 16:24:59 +0300

    google+ +)

---
+++ SLVGViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLVGViewController.h
+//  Social Networks
+//
+//  Created by Oleksiy on 8/17/14.
+//  Copyright (c) 2014 SoftServe Lv-120. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLVGViewController : UIViewController
+
+@end
---
+++ SLVGViewController.m
@@ -0,0 +1,13 @@
+//
+//  SLVGViewController.m
+//  Social Networks
+//
+//  Created by Oleksiy on 8/17/14.
+//  Copyright (c) 2014 SoftServe Lv-120. All rights reserved.
+//
+
+#import "SLVGViewController.h"
+
+@implementation SLVGViewController
+
+@end
